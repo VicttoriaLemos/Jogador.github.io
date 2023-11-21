91b97a8c268612001e63ddb1ec13ff3d3add9eed
VicttoriaLemos <146143953+VicttoriaLemos@users.noreply.github.com>
2023-11-21 19:11:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -217,42 +217,7 @@ app.get("/listar", function (req, res) {
 })
 
 app.get("/", function (req, res) {
-    res.send(`
-    <!DOCTYPE html>
-    <html lang="pt-br">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Option Select</title>
-        <link rel="icon" type="image/x-icon" href="icon.png">
-        <link rel="stylesheet" type="text/css" href="/estilo.css">
-    </head>
-<body class="site">
-    <div class="bg1">
-    <h1></h1>
-    </div>
-    <div class="site-content bg2">
-        <nav>
-            <ul class="local">
-                <li>
-                    <a class="btn btn-neon" title="Criar Perfil" href="http://localhost:8081/cadastro">CRIAR
-                        PERFIL</a>
-                </li>
-                <li>
-                    <a class="btn btn-neon" title="Acessar Perfil" href="http://localhost:8081/listar">ACESSAR
-                        PERFIL</a>
-                </li>
-            </ul>
-        </nav>
-    </div> 
-    <footer>
-    <a></a>
-    </footer>
-</body>
-</html>
-    
-    `);
+    res.sendFile(__dirname + "/index.html")
 });
 
 app.get("/deletar/:id_jogador", function (req, res) {
